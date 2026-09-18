42a3dd8a6c16f0c2a907796fface71a6b7da7112
togawa.ichiro <togawa.ichiro@kyla.co.jp>
2026-09-18 17:02:17 +0900

    Add new terms and concepts for the SF Fantasy Worldline
    - Created entries for various terms including "ひなた場", "フィクション科学時代", "信仰", "光の遅れ", "割り算", "原初の時代", "器", "大災厄", "崩壊時代", "情報体", "戻り船", "数合わせ", "森光", "現代科学先端時代", "白い年", "積み過ぎ", "端子", "系統外", "継ぎ火", "表と裏", "配る者が支配する", "霊体", and "魔力".
    - Each entry includes definitions, classifications, and contextual information relevant to the SF Fantasy Worldline.

--- tools/schema.py
+++ tools/schema.py
@@ -89,9 +89,11 @@ class Character(Base):
     end: Mapped[datetime | None] = mapped_column(DateTime)
 
 
-class Actor(Base):
+class Object(Base):
     """
     novels/actors/{root_place_name}/**/{actor_name}/actor.md
+
+    舞台装置として、個、または多数の存在として行動する。
     """
     __tablename__ = "actor"
 
@@ -107,7 +109,7 @@ class Actor(Base):
     end: Mapped[datetime | None] = mapped_column(DateTime)
 
 
-class ActorPlace(Base):
+class ObjectPlace(Base):
     """
     {actor_name}/places/{yyyymmddhhmmss}_{place_name}.md
     """
@@ -121,13 +123,59 @@ class ActorPlace(Base):
     end: Mapped[datetime | None] = mapped_column(DateTime)
 
 
-class Action(Base):
+class ObjectAction(Base):
     """
     {actor_name}/actions/{yyyymmddhhmmss}_{action_name}.md
     """
     __tablename__ = "actor_event"
 
     actor_id: Mapped[str] = mapped_column(String, ForeignKey("actor.id"))
+    event_id: Mapped[str] = mapped_column(String, ForeignKey("event.id"))
+
+    start: Mapped[datetime | None] = mapped_column(DateTime)
+    end: Mapped[datetime | None] = mapped_column(DateTime)
+
+
+class Character(Base):
+    """
+    novels/characters/{root_place_name}/{character_name}.md
+
+    一人ひとり、性格を持った個人として行動する。ストーリーの人称として表現される。
+    """
+
+    __tablename__ = "character"
+
+    name: Mapped[str] = mapped_column(String)
+    read: Mapped[str] = mapped_column(String)
+    kind: Mapped[str] = mapped_column(String)
+
+    root_place_id: Mapped[str | None] = mapped_column(String, ForeignKey("place.id"))
+
+    start: Mapped[datetime | None] = mapped_column(DateTime)
+    end: Mapped[datetime | None] = mapped_column(DateTime)
+
+
+class CharacterPlace(Base):
+    """
+    {character_name}/places/{yyyymmddhhmmss}_{place_name}.md
+    """
+
+    __tablename__ = "character_place"
+
+    character_id: Mapped[str] = mapped_column(String, ForeignKey("character.id"))
+    place_id: Mapped[str] = mapped_column(String, ForeignKey("place.id"))
+
+    start: Mapped[datetime | None] = mapped_column(DateTime)
+    end: Mapped[datetime | None] = mapped_column(DateTime)
+
+
+class CharacterAction(Base):
+    """
+    {character_name}/actions/{yyyymmddhhmmss}_{action_name}.md
+    """
+    __tablename__ = "character_event"
+
+    character_id: Mapped[str] = mapped_column(String, ForeignKey("character.id"))
     event_id: Mapped[str] = mapped_column(String, ForeignKey("event.id"))
 
     start: Mapped[datetime | None] = mapped_column(DateTime)
